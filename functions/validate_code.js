6ddc9bcb33f41d976d6f056e2c7f420069d5536a
ravenmindmedia <info@ravenmind.net>
2025-05-01 12:21:41 +0300

    Update validate_code.js

--- functions/validate_code.js
+++ functions/validate_code.js
@@ -2,9 +2,10 @@ export async function onRequestPost({ request }) {
   const formData = await request.formData();
   const code = formData.get("code");
 
+  // Kod doğrulama mantığı
   if (code === "ABC123") {
-    return new Response("Kod geçerli! Dosya indirme linki: https://ranemnind.net/oyun.zip");
+    return new Response("Kod geçerli! İndirme bağlantısı: https://ranemnind.net/oyun.zip");
   } else {
-    return new Response("Kod geçersiz", { status: 403 });
+    return new Response("Kod geçersiz.", { status: 403 });
   }
 }
